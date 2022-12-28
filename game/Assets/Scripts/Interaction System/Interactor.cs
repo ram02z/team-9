c5d3b02e6c1d772f751c202e66f5be69a14b22b9
psykl5 <psykl5@nottingham.ac.uk>
2022-12-28 17:12:08 +0000

    feat: developed interaction system

---
+++ game/Assets/Scripts/Interaction System/Interactor.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+//Script that gets the interaction object, sets a radius of size and detects colliders
+//When an interaction ocuurs an action is performed
+public class Interactor : MonoBehaviour
+{
+    //Gets position on interaction point
+    [SerializeField] private Transform _interactionPoint;
+    //Sets radius of interaction field
+    [SerializeField] private float _interactionPointRadius = 0.3f;
+    //Sets a layer so the script only interacts with objects in set layer (Should be "interaction" layer)
+    [SerializeField] private LayerMask _interactionMask;
+
+    //Detects a max of 4 colliders at once
+    private readonly Collider[] _colliders = new Collider[3];
+    //(For testing purpouses) Displays the number of interactable objects colliding with the interaction point
+    [SerializeField] private int _numFound;
+    private I_Interactable _interactable;
+
+    
+
+    private void Update()
+    {
+        //Gets the number of interactable objects colliding with the interaction point
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders,
+            _interactionMask);
+        //If an object is found
+        if (_numFound > 0)
+        {
+            // Will get the nearest (first registered) object 
+            var interactable = _colliders[0].GetComponent<I_Interactable>();
+            //If "e" key is pressed
+            if (interactable != null && Input.GetKeyDown(KeyCode.E))
+            {
+                //Peform the interaction task
+                interactable.Interact(this);
+            }
+        }
+    }
+
+    //(For testing purpouses) Will display a wired sphere in edit mode to visualises the area of interaction
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position,_interactionPointRadius);
+    }
+}
+
+
+
